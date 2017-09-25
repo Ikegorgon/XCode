9a5e0a89492f2e8369335fd7a47f68b1d97c98fd
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-09-25 12:45:20 -0600

    Playground with Strings assignment

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -4,3 +4,17 @@ import UIKit
 
 var str = "Hello, playground"
 print (str)
+var number = 2+3*4-8
+var int = 9%4
+str = "Hello, playground, how's it going?"
+print (str)
+print (str + " Good, Thanks for asking")
+var char = str.characters.count
+print (char)
+var str2 = "Hello, playground, how's it going?"
+if (str == str2) {
+    var yes = "yes"
+    print (yes)
+} else {
+    print ("no")
+}
